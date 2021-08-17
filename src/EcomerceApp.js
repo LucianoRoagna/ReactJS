986cc67c670e76c6ff64eadcc2979d56f5fb5d22
Luciano Roagna <lucianoroagna@gmail.com>
2021-08-17 15:17:43 -0300

    NavBar

---
+++ src/EcomerceApp.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import NavBarComp from './components/NavBar/NavBarComp'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <NavBarComp />
+     
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+//NO SE TOCA
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import EcomerceApp from './EcomerceApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <EcomerceApp /> ,
   document.getElementById('root')
 );
 
