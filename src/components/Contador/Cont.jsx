7c0328f47069cf4fe986cccf1501e755d221a68e
Luciano Roagna <lucianoroagna@gmail.com>
2021-08-24 16:27:53 -0300

    Agregado de card desafio 05

--- src/components/Contador/Cont.jsx
+++ src/components/Contador/Cont.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import './Cont.css'
 
 function Cont({initial,stock,onAdd}) {
   const [cont, setCont] = useState(initial);
@@ -20,15 +23,32 @@ function Cont({initial,stock,onAdd}) {
   
 
   return (
-    <div>
-      <button onClick={add}>+</button>
-      <label> {cont} </label>
-      <button onClick={remove}>-</button>
-
-      <button onClick={handlerOnAdd}>Agregar</button>
-    </div>
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Producto</Card.Title>
+    <Card.Text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consequuntur dolorum corrupti quasi dolorem non tenetur .
+    </Card.Text>
+    <Button variant="primary" onClick={add}>+</Button>
+    <label>{cont}</label>
+    <Button variant="primary" onClick={remove}>-</Button>
+<Button variant="primary" onClick={handlerOnAdd}>Agregar</Button>
+  </Card.Body>
+</Card>
+   
   );
   }
 
 
 export default Cont;
+
+
+
+// <div>
+//<button onClick={add}>+</button>
+//<label> {cont} </label>
+//<button onClick={remove}>-</button>
+
+//<button onClick={handlerOnAdd}>Agregar</button>
+//</div>
